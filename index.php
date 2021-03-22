cd9127373ce20a93b12a69d0e5821abc93e027cc
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-03-22 12:54:53 -0400

    index.php returns all locations within a certain radius of location
    Features
     - Index.php returns the location of all facilities within a given radius of a coordinate

--- index.php
+++ index.php
@@ -1,27 +1,33 @@
 <?php
-	include 'SERVER.php'
-	$lat = $_GET['lat'];
-	floatval($lat);
+	include 'SERVER.php';
+	$lat = doubleval($_GET['lat']);
     
-    $lon = $_GET['lon'];
-	floatval($lon);
+    $lon = doubleval($_GET['lon']);
 
-    $radius = $_GET['rad'];
-	floatval($radius);
+    $radius = doubleval($_GET['rad']);
 
     $conn = new mysqli(DB_SERVER, DB_NAME, DB_PASSWORD, DB_DATABASE);
 
-	$command = $conn->prepare("SELECT `id`, `longitude`, `latitude`, `type`, `upVote`, `downVote`, `visitors` FROM `locations` WHERE `longitude` BETWEEN ? AND ? AND `latitude` BETWEEN ? AND ? ");
+	$command = $conn->prepare("SELECT `id`, `longitude`, `latitude`, `type`, `upVote`, `downVote`, `visitors` FROM `locations` WHERE `longitude` BETWEEN ? AND ? And `latitude` BETWEEN ? AND ?");
 
-	$command->bind_param("iiii", $lon - $radius, $lon + $radius, $lat - $radius, $lat + $radius);
+	$lonMin = $lon - $radius;
+	$lonMax = $lon + $radius;
+
+	$latMin = $lat - $radius;
+	$latMax = $lat + $radius;
+
+	$command->bind_param("dddd", $lonMin, $lonMax, $latMin, $latMax);
+
 
 	$command->execute();
 
 	$command->store_result();
 
-	$command->bind_result($location);
-
+	$command->bind_result($id, $longitude, $latitude, $type, $upVote, $downVote, $visitors);
 	while ($command->fetch()){
-		echo $location;
+
+		//$result = array("question_ID"=>$question_ID,"Question"=>$question);
+
+		echo "id:".$id.",Lon:".$longitude.",lat:".$latitude.",type:".$type.",upVote:".$upVote.",downVote:".$downVote.",visitors:".$visitors."<br>";
 	}
 ?>
